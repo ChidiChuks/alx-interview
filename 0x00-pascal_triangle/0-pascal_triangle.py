c42a47fa2b419a3cb6ef2388c298e2269c8e8b75
Chidiebere Chukwuma <chukschiboy@gmail.com>
2024-01-28 11:09:57 +0000

    returns a list of lists of integers representing

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
+"""
+contains the Pascal's Triangle function
+"""
+
+
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-
-    triangle = [[1]]
-
-    for i in range(1, n):
-        row = [1]
-        prev_row = triangle[i - 1]
-
-        for j in range(1, i):
-            row.append(prev_row[j - 1] + prev_row[j])
-
-        row.append(1)
+    """ returns a list of lists of integers representing
+        the Pascal’s triangle of n
+    """
+    triangle = []
+    for i in range(1, n+1):
+        row = []
+        for j in range(i):
+            if j == 0 or j == i-1:
+                n = 1
+                row.append(n)
+            else:
+                n = triangle[i-2][j-1] + triangle[i-2][j]
+                row.append(n)
         triangle.append(row)
 
     return triangle
