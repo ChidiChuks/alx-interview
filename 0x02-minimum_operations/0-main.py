c0f87161c33b72ffc8a8f47185b32f8c1fe1f74e
Chidiebere Chukwuma <chukschiboy@gmail.com>
2024-01-31 00:14:19 +0000

    def minOperations(n)

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -5,12 +5,8 @@ Main file for testing
 
 minOperations = __import__('0-minoperations').minOperations
 
-
-n = 2 * 2 * 2
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
-
-n = 44
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
