39848a6e15433530daf4ee1e23699283c28182c6
Chidiebere Chukwuma <chukschiboy@gmail.com>
2024-01-31 00:25:35 +0000

    update on minOperations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,26 +4,19 @@ Script that computes a minimum operation
 check all
 """
 
-
 def minOperations(n):
     if n <= 1:
         return n
 
     operations = 0
-    clipboard = 1  # Initialize clipboard with one 'H'
-    total_H = 1    # Initially, there's one 'H' in the file
+    divisor = 2
 
-    while total_H < n:
-        if n % total_H == 0:
-            # If the number of 'H' needed is a multiple of the current total_H,
-            # perform Copy All operation to duplicate the current sequence
-            operations += 1
-            clipboard = total_H
-            total_H *= 2
+    while n > 1:
+        if n % divisor == 0:
+            n //= divisor
+            operations += divisor
         else:
-            # Paste from clipboard
-            operations += 1
-            total_H += clipboard
+            divisor += 1
 
     return operations
 
@@ -33,3 +26,4 @@ print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
