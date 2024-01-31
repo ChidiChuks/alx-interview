e31269e188865d1e3b386ac4efcbd1cda322254e
Chidiebere Chukwuma <chukschiboy@gmail.com>
2024-01-31 00:33:50 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
 """
-Script that computes a minimum operation
-check all
+Script that computes a minimum operations
+needed in a CopyAll - Paste task
 """
 
+
 def minOperations(n):
-    if n <= 1:
-        return n
-
-    operations = 0
-    divisor = 2
-
+    """
+    Method for computing the minimum number
+    of operations for task Copy All and Paste
+    Args:
+        n: input value
+    Return: the sum of the operations
+    """
+    if n < 2:
+        return 0
+    factor_sum = 0
+    i = 2  # Start with the first prime number
     while n > 1:
-        if n % divisor == 0:
-            n //= divisor
-            operations += divisor
+        if n % i == 0:
+            factor_sum += i
+            n //= i  # Use integer division
         else:
-            divisor += 1
+            i += 1
+    return factor_sum
 
-    return operations
-
-# Example usage:
-n = 9
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-n = 12
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
+# Example usage
+result = minOperations(12)
+print("Minimum operations:", result)
